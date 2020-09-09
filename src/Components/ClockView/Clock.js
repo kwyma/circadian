5c09fc7cb88717d19669f999de0ddb5c6c64af31
kwyma <kwyma@uw.edu>
2020-09-08 20:12:00 -0700

    added clock files

---
+++ src/Components/ClockView/Clock.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './Clock.css';
+
+class Clock extends Component {
+  render() {
+    return (
+      <span class="Clock__shape"></span>
+    );
+  }
+}
+
+export default Clock;
