b6e5dfeb6f805faf689c40ad9dfcb32368010a83
kwyma <kwyma@uw.edu>
2020-09-09 22:25:59 -0700

    clock syncs with time

--- src/Components/DayView/ClockView/Clock.js
+++ src/Components/DayView/ClockView/Clock.js
@@ -2,9 +2,53 @@ import React, { Component } from 'react';
 import './Clock.css';
 
 class Clock extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentDate: new Date()
+    };
+  }
+
+  componentDidMount() {
+    setInterval(() => this.updateTime(), 500);
+  }
+
+  updateTime = () => {
+    this.setState({
+      currentDate: new Date()
+    })
+  }
+
   render() {
+    const secondsRatio = this.state.currentDate.getSeconds() / 60;
+    const minutesRatio = (secondsRatio + this.state.currentDate.getMinutes()) / 60;
+    const hoursRatio = (minutesRatio + this.state.currentDate.getHours())/ 12;
+
+    const minuteHandStyle = {
+      transform: "translateX(-50%) rotate(calc(" + minutesRatio * 360 + "deg))"
+    };
+    const hourHandStyle = {
+      transform: "translateX(-50%) rotate(calc(" + hoursRatio * 360 + "deg))"
+    };
+
     return (
-      <span class="Clock__shape"></span>
+      <div className="Clock__Shape">
+        <div className="Clock__Hand Clock__Hand-minute" style={minuteHandStyle}></div>
+        <div className="Clock__Hand Clock__Hand-hour" style={hourHandStyle}></div>
+        <div className="Clock__Number Clock__Number-1">1</div>
+        <div className="Clock__Number Clock__Number-2">2</div>
+        <div className="Clock__Number Clock__Number-3">3</div>
+        <div className="Clock__Number Clock__Number-4">4</div>
+        <div className="Clock__Number Clock__Number-5">5</div>
+        <div className="Clock__Number Clock__Number-6">6</div>
+        <div className="Clock__Number Clock__Number-7">7</div>
+        <div className="Clock__Number Clock__Number-8">8</div>
+        <div className="Clock__Number Clock__Number-9">9</div>
+        <div className="Clock__Number Clock__Number-10">10</div>
+        <div className="Clock__Number Clock__Number-11">11</div>
+        <div className="Clock__Number Clock__Number-12">12</div>
+      </div>
     );
   }
 }
