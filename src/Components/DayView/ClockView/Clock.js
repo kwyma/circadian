ef39389259cb9cd587bf00ad5976c5408dabadd7
kwyma <kwyma@uw.edu>
2020-09-16 17:57:27 -0700

    added clock number counterrotation

--- src/Components/DayView/ClockView/Clock.js
+++ src/Components/DayView/ClockView/Clock.js
@@ -36,18 +36,43 @@ class Clock extends Component {
       <div className="Clock__Shape">
         <div className="Clock__Hand Clock__Hand-minute" style={minuteHandStyle}></div>
         <div className="Clock__Hand Clock__Hand-hour" style={hourHandStyle}></div>
-        <div className="Clock__Number Clock__Number-1">1</div>
-        <div className="Clock__Number Clock__Number-2">2</div>
-        <div className="Clock__Number Clock__Number-3">3</div>
-        <div className="Clock__Number Clock__Number-4">4</div>
-        <div className="Clock__Number Clock__Number-5">5</div>
-        <div className="Clock__Number Clock__Number-6">6</div>
-        <div className="Clock__Number Clock__Number-7">7</div>
-        <div className="Clock__Number Clock__Number-8">8</div>
-        <div className="Clock__Number Clock__Number-9">9</div>
-        <div className="Clock__Number Clock__Number-10">10</div>
-        <div className="Clock__Number Clock__Number-11">11</div>
-        <div className="Clock__Number Clock__Number-12">12</div>
+        
+        <div className="Clock__NumberContainer Clock__NumberContainer-1">
+          <span className="Clock__Number Clock__Number-1">1</span>
+        </div>
+        <div className="Clock__NumberContainer Clock__NumberContainer-2">
+          <span className="Clock__Number Clock__Number-2">2</span>
+        </div>
+        <div className="Clock__NumberContainer Clock__NumberContainer-3">
+          <span className="Clock__Number Clock__Number-3">3</span>
+        </div>
+        <div className="Clock__NumberContainer Clock__NumberContainer-4">
+          <span className="Clock__Number Clock__Number-4">4</span>
+        </div>
+        <div className="Clock__NumberContainer Clock__NumberContainer-5">
+          <span className="Clock__Number Clock__Number-5">5</span>
+        </div>
+        <div className="Clock__NumberContainer Clock__NumberContainer-6">
+          <span className="Clock__Number Clock__Number-6">6</span>
+        </div>
+        <div className="Clock__NumberContainer Clock__NumberContainer-7">
+          <span className="Clock__Number Clock__Number-7">7</span>
+        </div>
+        <div className="Clock__NumberContainer Clock__NumberContainer-8">
+          <span className="Clock__Number Clock__Number-8">8</span>
+        </div>
+        <div className="Clock__NumberContainer Clock__NumberContainer-9">
+          <span className="Clock__Number Clock__Number-9">9</span>
+        </div>
+        <div className="Clock__NumberContainer Clock__NumberContainer-10">
+          <span className="Clock__Number Clock__Number-10">10</span>
+        </div>
+        <div className="Clock__NumberContainer Clock__NumberContainer-11">
+          <span className="Clock__Number Clock__Number-11">11</span>
+        </div>
+        <div className="Clock__NumberContainer Clock__NumberContainer-12">
+          <span className="Clock__Number Clock__Number-12">12</span>
+        </div>
       </div>
     );
   }
